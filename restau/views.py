4c4d169a25a019b3aa95b85623867654509bcde9
Shrey Sumariya <you@example.com>
2021-07-21 22:30:15 +0530

    some changes done

--- restau/views.py
+++ restau/views.py
@@ -6,25 +6,27 @@ from rest_framework import status
 # Create your views here.
 from restau.models import Restaurant, Recipe
 from . import serializers
+from django.http.response import JsonResponse
 
 
 class Restaurants(APIView):
 
-    def get(self,request):
+    def get(self, request):
         restaurants = Restaurant.objects.all()
-        serializer = serializers.RestaurantSerializer(restaurants,many=True)
-        return Response(serializer.data)
+        serializer = serializers.RestaurantSerializer(restaurants, many=True)
+        return JsonResponse(serializer.data)
 
-    def post(self,request):
+    def post(self, request):
         serializer = serializers.RestaurantSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class RestaurantDetail(APIView):
 
-    def get(self,request,restaurant_id):
+    def get(self, request, restaurant_id):
         try:
             restaurant = Restaurant.objects.get(pk=restaurant_id)
         except Restaurant.DoesNotExist:
@@ -32,18 +34,19 @@ class RestaurantDetail(APIView):
         serializer = serializers.RestaurantSerializer(restaurant)
         return Response(serializer.data)
 
-    def put(self,request,restaurant_id):
+    def put(self, request, restaurant_id):
         try:
             restaurant = Restaurant.objects.get(pk=restaurant_id)
         except Restaurant.DoesNotExist:
             raise Http404
-        serializer = serializers.RestaurantSerializer(restaurant,data=request.data)
+        serializer = serializers.RestaurantSerializer(
+            restaurant, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_205_RESET_CONTENT)
-        return Response(serializer.errors,status=status.HTTP_304_NOT_MODIFIED)
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_304_NOT_MODIFIED)
 
-    def delete(self,request,restaurant_id):
+    def delete(self, request, restaurant_id):
         try:
             restaurant = Restaurant.objects.get(pk=restaurant_id)
         except Restaurant.DoesNotExist:
@@ -51,14 +54,15 @@ class RestaurantDetail(APIView):
         restaurant.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class Recipes(APIView):
 
-    def get(self,request,restaurant_id):
+    def get(self, request, restaurant_id):
         recipes = Recipe.objects.filter(restaurant_id=restaurant_id)
-        serializer = serializers.RecipeSerializer(recipes , many=True)
+        serializer = serializers.RecipeSerializer(recipes, many=True)
         return Response(serializer.data)
 
-    def post(self,request,restaurant_id):
+    def post(self, request, restaurant_id):
         try:
             Restaurant.objects.get(pk=restaurant_id)
         except Restaurant.objects.DoesNotExist:
@@ -66,34 +70,38 @@ class Recipes(APIView):
         serializer = serializers.RecipeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(restaurant_id=restaurant_id)
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 class RecipeDetail(APIView):
 
-    def get(self,request,restaurant_id,recipe_id):
+    def get(self, request, restaurant_id, recipe_id):
         try:
-            recipe = Recipe.objects.get(restaurant_id=restaurant_id,pk=recipe_id)
+            recipe = Recipe.objects.get(
+                restaurant_id=restaurant_id, pk=recipe_id)
         except Recipe.DoesNotExist:
             raise Http404
         serializer = serializers.RecipeSerializer(recipe)
         return Response(serializer.data)
 
-    def delete(self,request,restaurant_id,recipe_id):
+    def delete(self, request, restaurant_id, recipe_id):
         try:
-            recipe = Recipe.objects.get(restaurant_id=restaurant_id,pk=recipe_id)
+            recipe = Recipe.objects.get(
+                restaurant_id=restaurant_id, pk=recipe_id)
         except Recipe.DoesNotExist:
             raise Http404
         recipe.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def put(self,request,restaurant_id,recipe_id):
+    def put(self, request, restaurant_id, recipe_id):
         try:
-            recipe = Recipe.objects.get(restaurant_id=restaurant_id,pk=recipe_id)
+            recipe = Recipe.objects.get(
+                restaurant_id=restaurant_id, pk=recipe_id)
         except Recipe.DoesNotExist:
             raise Http404
-        serializer = serializers.RecipeSerializer(recipe,data=request.data)
+        serializer = serializers.RecipeSerializer(recipe, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data,status=status.HTTP_205_RESET_CONTENT)
-        return Response(serializer.errors,status=status.HTTP_304_NOT_MODIFIED)
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_304_NOT_MODIFIED)
